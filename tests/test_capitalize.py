922dd77f11650f7d12ba7ca7b9e1bb06c950906f
DzmitrySha <ravenbydev@gmail.com>
2022-03-01 16:28:28 +0300

    modified test_capitalize.py

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,10 +1,6 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
-
